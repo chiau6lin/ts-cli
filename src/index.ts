b1cd2522eca3f1df30fa614add1385710f656048
Jamie Lin <jamielin0.tw@gmail.com>
2023-10-20 06:07:15 +0800

    feat: add command example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
 #!/usr/bin/env node
 
-const hello = () => {}
+import { Command } from "commander"
+
+const program = new Command()
+
+program
+  .argument("<string>", "string to log")
+  .option("-c, --capitalize", "Capitalize the message")
+  .action(
+    (
+      message: string,
+      opts: {
+        capitalize?: boolean
+      }
+    ) => {
+      if (opts.capitalize) {
+        console.log(message.toUpperCase())
+      } else {
+        console.log(message)
+      }
+    }
+  )
+  .description("Say Hi")
+
+program
+  .command("add <numbers...>")
+  .action((numbers: number[]) => {
+    const total = numbers.reduce((a, b) => Number(a) + Number(b), 0)
+    console.log(`Total: ${total}`)
+  })
+  .description("Add numbers and log the total")
+
+program
+.command("get max number <numbers...>")
+.action((numbers: number[]) => {
+  const max = Math.max(...numbers)
+  console.log(`Max: ${max}`)
+})
+.description("Add numbers and log the total")
+
+program.parse(process.argv)
